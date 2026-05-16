9d5e7bbb6d187d2daaaa0f646438872e96fff906
Godha Keerthi <workingkeerthispg@gmail.com>
2026-05-16 17:29:06 +0530

    Implement binary search to find minimum in rotated array

---
+++ Solution_leet_48.java
@@ -0,0 +1,24 @@
+class Solution_leet_48{
+    public int findMin(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            
+            if (nums[mid] > nums[right]) {
+                // The minimum element must be in the right unsorted part
+                left = mid + 1;
+            } else if (nums[mid] < nums[right]) {
+                // The right part is sorted, so mid could be the minimum or to its left
+                right = mid;
+            } else {
+                // When nums[mid] == nums[right], we can't decide the side.
+                // Safely reduce the search space by shifting the right boundary.
+                right--;
+            }
+        }
+        
+        return nums[left];
+    }
+}
