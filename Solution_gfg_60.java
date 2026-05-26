18a7edc7ab1ad5e848ea8c5e6459cc52c1305ae2
Godha Keerthi <workingkeerthispg@gmail.com>
2026-05-26 10:54:24 +0530

    Create Solution_gfg_60.java

---
+++ Solution_gfg_60.java
@@ -0,0 +1,20 @@
+class Solution_gfg_60 {
+    int minToggle(int[] arr) {
+        int ones = 0;
+        int toggles = 0;
+        
+        for (int num : arr) {
+            if (num == 1) {
+                // Encountered a 1, increment our 1s counter
+                ones++;
+            } else {
+                // Encountered a 0, decide the cheaper option:
+                // 1. Flip this 0 to 1 -> toggles + 1
+                // 2. Keep this 0, flip all previous 1s -> ones
+                toggles = Math.min(toggles + 1, ones);
+            }
+        }
+        
+        return toggles;
+    }
+}
