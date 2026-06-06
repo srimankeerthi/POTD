5adedc532c89cd90ad27442c6cd2b81eeb32372f
Godha Keerthi <workingkeerthispg@gmail.com>
2026-06-06 11:16:48 +0530

    Create Solution_leet_72.java

---
+++ Solution_leet_72.java
@@ -0,0 +1,26 @@
+class Solution_leet_72 {
+    public int[] leftRightDifference(int[] nums) {
+        int n =nums.length;
+
+        int[] left = new int[n];
+        int[] right = new int[n];
+        int[] res = new int[n];
+
+        for ( int i =1;i<n;i++){
+            left[i]=left[i-1]+nums[i-1];
+
+        }
+        
+
+        
+     
+        for(int i =n-2;i>=0;i--){
+            right[i]=right[i+1]+ nums[i+1];
+
+        }
+        for(int i = n-1;i>=0;i--){
+            res[i]=Math.abs(left[i]-right[i]);
+        }
+        return res;
+    }
+}
